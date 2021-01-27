9a8808807d8e03abea7ddd8053c2401a2937a649
Daniel Leme Junior <daniel.dlj@hotmail.com>
2021-01-27 18:06:07 -0300

    [ADD]  Spring Initializr

---
+++ SpringBootUpvote/src/main/java/com/DanielDLJ/SpringBootUpvote/SpringBootUpvoteApplication.java
@@ -0,0 +1,13 @@
+package com.DanielDLJ.SpringBootUpvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUpvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUpvoteApplication.class, args);
+	}
+
+}
---
+++ SpringBootUpvote/src/test/java/com/DanielDLJ/SpringBootUpvote/SpringBootUpvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DanielDLJ.SpringBootUpvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUpvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
